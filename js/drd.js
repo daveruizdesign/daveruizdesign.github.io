18806033e8d88068eaf5e5ff37bec642e96896fe
Nol Franklin <nfranklin@salesforce.com>
2016-12-12 16:50:04 -0800

    File updateS

--- js/drd.js
+++ js/drd.js
@@ -12,3 +12,22 @@ $grid.isotope({
 	sortBy : 'weight',
 	sortAscending: true
 });
+
+
+$("#drd__form").validate({
+	rules: {
+		name: "required",
+		email: {
+			required: true,
+			email: true
+		}
+	},
+	messages: {
+		name: "Please specify your name",
+		email: {
+			required: "We need your email address to contact you",
+			email: "Your email address must be in the format of name@domain.com"
+		}
+	},
+	errorElement: "div",
+});
