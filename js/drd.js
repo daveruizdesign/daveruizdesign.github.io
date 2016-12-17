8684036870fb717576ecf2e7a0c129baec65abb6
Nol Franklin <nfranklin@salesforce.com>
2016-12-17 13:23:42 -0800

    Form script updates

--- js/drd.js
+++ js/drd.js
@@ -13,18 +13,26 @@ $grid.isotope({
 
 $("#drd__form").validate({
 	rules: {
-		name: "required",
+		name: {
+			required: true,
+			minlength: 2
+		},
 		email: {
 			required: true,
 			email: true
-		}
+		},
+		subject: {
+			required: true,
+			minlength: 2
+		},
 	},
 	messages: {
 		name: "Please specify your name",
 		email: {
 			required: "We need your email address to contact you",
 			email: "Your email address must be in the format of name@domain.com"
-		}
+		},
+		subject: "Please specify a subject"
 	},
 	errorElement: "div",
 });
