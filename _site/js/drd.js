03d601730897b01bb2902d0dd31d10a429b9f5ec
Nol Franklin <nfranklin@salesforce.com>
2017-01-26 10:59:16 -0800

    Resize fix for work

--- _site/js/drd.js
+++ _site/js/drd.js
@@ -1,71 +1,72 @@
-/* WOW */
+$( document ).ready(function() {
 
-new WOW().init();
+	/* WOW */
 
-skrollr.init({
-    render: function(data) {
-        //Log the current scroll position.
-        //console.log(data.curTop);
-    }
-});
+	new WOW().init();
 
-/* Form validation */
+	skrollr.init({
+	    render: function(data) {
+	        //Log the current scroll position.
+	        //console.log(data.curTop);
+	    }
+	});
 
-$("#drd__form").validate({
-	rules: {
-		name: {
-			required: true,
-			minlength: 2
-		},
-		email: {
-			required: true,
-			email: true
-		},
-		subject: {
-			required: true,
-			minlength: 2
-		},
-		message: {
-			required: true,
-			minlength: 2
-		}
-	},
-	messages: {
-		name: "Please specify your name",
-		email: {
-			required: "We need your email address to contact you",
-			email: "Please enter a valid email"
-		},
-		subject: "Please specify a subject",
-		message: "Please write a message"
-	},
-	errorElement: "div",
-});
+	/* Form validation */
 
-
-
-/* Gallery layout based on viewport */
-
-//enquire.register("screen and (min-width: 0px) and (max-width:1649px)", {
-enquire.register("screen and (min-width: 0px) and (max-width:1439px)", {
-	match : function() {
-		var $grid = $('.work-list');
-		$grid.isotope({
-			itemSelector: '.card--work',
-			layoutMode: 'packery',
-			getSortData: {
-				number: '[data-number] parseInt',
+	$("#drd__form").validate({
+		rules: {
+			name: {
+				required: true,
+				minlength: 2
 			},
-			sortBy : 'number',
-			sortAscending: true
-		});
-		$(window).on('resize', function(){
-			$grid.isotope('reloadItems');
-			console.log("Window resized.");
-		});
-	},
-	unmatch : function() {
-		var $grid = $('.work-list');
-		$grid.isotope('destroy');
-	}
+			email: {
+				required: true,
+				email: true
+			},
+			subject: {
+				required: true,
+				minlength: 2
+			},
+			message: {
+				required: true,
+				minlength: 2
+			}
+		},
+		messages: {
+			name: "Please specify your name",
+			email: {
+				required: "We need your email address to contact you",
+				email: "Please enter a valid email"
+			},
+			subject: "Please specify a subject",
+			message: "Please write a message"
+		},
+		errorElement: "div",
+	});
+
+
+
+	/* Gallery layout based on viewport */
+
+	enquire.register("(min-width: 0px) and (max-width:1439px)", {
+		match : function() {
+			var $grid = $('.work-list');
+			$grid.isotope({
+				itemSelector: '.card--work',
+				layoutMode: 'packery',
+				getSortData: {
+					number: '[data-number] parseInt',
+				},
+				sortBy : 'number',
+				sortAscending: true
+			});
+
+		},
+		unmatch : function() {
+			var $grid = $('.work-list');
+			$grid.isotope('destroy');
+			console.log("Unmatched");
+		}
+	});
+
 });
